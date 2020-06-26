6361689921032964319f79ceecd257a7dc62c2cc
svyatoslavlp <svyatoslavlp@mail.ru>
2020-06-26 22:52:44 +0500

    Структура, 23_00 по видео начало gulpfile

---
+++ gulpfile.js
@@ -0,0 +1,54 @@
+let project_folder = "dist"; // папка с готовыми к выгрузке файлами
+let sourse_folder = "_src"; // папка с исходными файлами
+
+let path = {
+  build: { // пути для сборки
+    html: project_folder + "/",
+    css: project_folder + "/css/",
+    js: project_folder + "/js/",
+    img: project_folder + "/img/",
+    fonts: project_folder + "/fonts/",
+  },
+  src: { // пути для исходников
+    html: sourse_folder + "/*.html",
+    css: sourse_folder + "/scss/style.scss",
+    js: sourse_folder + "/js/script.js",
+    img: sourse_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+    fonts: sourse_folder + "/fonts/*.ttf",
+  },
+  watch: { // пути для отслеживания
+    html: sourse_folder + "/**/*.html",
+    css: sourse_folder + "/scss/**/*.scss",
+    js: sourse_folder + "/js/**/*.js",
+    img: sourse_folder + "/img/**/*.{jpg,png,svg,gif,ico,webp}",
+  },
+  clean: "./" + project_folder + "/" // путь для очистки сборки перед новой сборкой
+};
+
+let { src, dest } = require("gulp"),
+  gulp = require("gulp"),
+  browsersync = require("browser-sync").create();
+
+function browserSync(params) {
+  browsersync.init({
+    server: {
+      baseDir: "./" + project_folder + "/" // путь основной директории для browsersync
+    },
+    port: 3000,
+    notify: false // отключение сообщений об обновлении страницы
+  });
+}
+
+function html() {
+  return src(path.src.html)
+    .pipe(dest(path.build.html)) // собрать новый html
+    .pipe(browsersync.stream()) // обновить браузер
+}
+
+let build = gulp.series(html);
+let watch = gulp.parallel(build, browserSync);
+
+exports.html = html;
+exports.build = build;
+exports.watch = watch;
+exports.default = watch;
