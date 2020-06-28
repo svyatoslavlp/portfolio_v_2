fe1126b2214ba546b1d4457294ccfdab2b8a33d6
svyatoslavlp <svyatoslavlp@mail.ru>
2020-06-28 19:33:41 +0500

    отслеживание scss и сборка в dist

--- gulpfile.js
+++ gulpfile.js
@@ -32,7 +32,8 @@ let { src, dest } = require("gulp"),
   gulp = require("gulp"),
   browsersync = require("browser-sync").create();
   fileinclude = require("gulp-file-include"),
-  del = require("del");
+  del = require("del"),
+  scss = require("gulp-sass");
 
 
 function browserSync(params) {
@@ -52,17 +53,31 @@ function html() {
     .pipe(browsersync.stream()); // обновить браузер
 }
 
+function css(params) {
+  return src(path.src.css)
+    .pipe(
+      scss({
+        outputStyle: "expanded"
+      })
+    )
+    .pipe(dest(path.build.css)) // собрать новый css
+    .pipe(browsersync.stream()); // обновить браузер
+}
+
 function watchFiles(params) {
   gulp.watch([path.watch.html], html);
+  gulp.watch([path.watch.css], css);
+
 }
 
 function clean(params) {
   return del(path.clean);
 }
 
-let build = gulp.series(clean, html);
+let build = gulp.series(clean, gulp.parallel(css, html));
 let watch = gulp.parallel(build, watchFiles, browserSync);
 
+exports.css = css;
 exports.html = html;
 exports.build = build;
 exports.watch = watch;
